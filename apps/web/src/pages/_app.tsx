856944be3fec44d9709c574daa2f9f33ee4eff38
Bu Kinoshita <6929565+bukinoshita@users.noreply.github.com>
2022-11-16 22:32:04 -0300

    feat(website): Add website

---
+++ apps/web/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+import 'design-system/styles.css';
+
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
