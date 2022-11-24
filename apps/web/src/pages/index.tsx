2e64604fda09034ad80fab3932bca24cf19c1955
Zeno Rocha <zno.rocha@gmail.com>
2022-11-24 09:15:27 -0300

    feat(web): Improve title layout and description copy

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -18,18 +18,18 @@ export default function Web() {
           <Pipe className="absolute -right-[200px] rotate-45 animate-[dash_8s]" />
           <Pipe className="absolute right-10 -top-[100px] animate-[dash_10s]" />
           <Pipe className="absolute left-0 -bottom-[130px] rotate-45 animate-[dash_12s]" /> */}
-          <div className="max-w-[745px] text-center">
+          <div className="max-w-[725px] text-center">
             <Heading
               className="before:bg-shine relative mb-8 !text-white/80 before:absolute before:left-0 before:top-0 before:w-full before:animate-[shine_2s_ease-in-out] before:bg-[length:200%] before:bg-clip-text before:text-transparent before:content-['The_next_generation_of_writing_emails'] "
               size="10"
             >
               The next generation of writing emails
             </Heading>
-            <Text size="5">
-              A collection of high-quality, unstyled components for creating
-              beautiful emails. It reduces the pain of coding responsive emails
-              with dark mode support.
-            </Text>
+            <div className="sm:px-20">
+              <Text size="5">
+                A collection of high-quality, unstyled components for creating beautiful emails using React and TypeScript. 
+              </Text>
+            </div>
           </div>
 
           <div className="mt-10 flex items-center justify-center gap-4">
