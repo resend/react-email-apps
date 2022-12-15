551212450dacd394b98377f6eb6a5f46f9e0b92d
Emerson Garrido <emersongarrido@hotmail.com.br>
2022-12-15 15:26:26 -0400

    update links template google play

---
+++ campaigns/emails/2022-11-17-survey-results.tsx
@@ -0,0 +1,97 @@
+import { Head } from '@react-email/head';
+import { Hr } from '@react-email/hr';
+import { Html } from '@react-email/html';
+import { Img } from '@react-email/img';
+import { Link } from '@react-email/link';
+import { Preview } from '@react-email/preview';
+import { Section } from '@react-email/section';
+import { Text } from '@react-email/text';
+import * as React from 'react';
+
+const Email = () => {
+  const link = 'https://rd8yncr0wr5.typeform.com/react-email';
+
+  return (
+    <Html>
+      <Head />
+      <Preview>
+        The results from the React Email questionnaire have been insane, with
+        206 responses so far.
+      </Preview>
+      <Section style={main}>
+        <Text style={body}>Hey, it's Zeno Rocha.</Text>
+        <Text style={body}>
+          The results from the React Email{' '}
+          <Link href={link}>questionnaire</Link> have been insane, with{' '}
+          <b>206 responses</b> so far.
+        </Text>
+        <Text style={body}>
+          There were a lot of interesting insights and ideas, so I wanted to
+          share some of them with you...
+        </Text>
+        <Text style={body}>
+          <b>1. What do you hate the most about email development?</b>
+        </Text>
+        <Text style={body}>
+          <em>
+            "Writing tables inside tables inside tables. My goodness, I hate it
+            so much."
+          </em>
+        </Text>
+        <Text style={body}>
+          <b>2. What email service provider do you use?</b>
+        </Text>
+        <Img
+          src="https://assets.react.email/campaigns/survey-email-provider.png"
+          alt="SendGrid: 52.3% / AWS SES: 35.5% / Mailchimp: 29.4%"
+          width="532"
+          height="232"
+        />
+        <Text style={body}>
+          <b>3. How do you develop email templates?</b>
+        </Text>
+        <Img
+          src="https://assets.react.email/campaigns/survey-email-dev.png"
+          alt="Pure HTML/CSS: 66.3% / MJML: 17.8% / Drag-n-drop: 12.9%"
+          width="532"
+          height="223"
+        />
+        <Text style={body}>
+          A lot of people also mentioned testing as one of the biggest pain
+          points:
+        </Text>
+        <Text style={body}>
+          <em>"Styling is hard, testing is even harder."</em>
+        </Text>
+        <Text style={body}>
+          We're working on something to help with this right now, and we plan to
+          share an update next week :)
+        </Text>
+        <Text style={body}>Thanks again for the amazing support so far.</Text>
+        <Hr />
+        <Text style={footer}>
+          You are receiving this email because you opted in at react.email
+        </Text>
+        <Text style={footer}>185 Royal Way - Upland, CA 91786</Text>
+      </Section>
+    </Html>
+  );
+};
+
+const main = {
+  backgroundColor: '#ffffff',
+};
+
+const body = {
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+};
+
+const footer = {
+  color: '#898989',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: '12px',
+};
+
+export default Email;
---
+++ campaigns/emails/2022-12-15-new-templates.tsx
@@ -0,0 +1,109 @@
+import { Head } from '@react-email/head';
+import { Hr } from '@react-email/hr';
+import { Html } from '@react-email/html';
+import { Img } from '@react-email/img';
+import { Link } from '@react-email/link';
+import { Preview } from '@react-email/preview';
+import { Section } from '@react-email/section';
+import { Text } from '@react-email/text';
+import * as React from 'react';
+
+const Email = () => {
+  const websiteLink = 'https://react.email';
+  const githubLink = 'https://github.com/zenorocha/react-email';
+  const airbnbLink = 'https://demo.react.email/preview/airbnb-review';
+  const dropboxLink = 'https://demo.react.email/preview/dropbox-reset-password';
+  const examplesLink = 'https://react.email/examples';
+  const contributeLink =
+    'https://github.com/zenorocha/react-email/issues?q=is%3Aissue+is%3Aopen+label%3A%22app%3A+demo%22';
+  const sourceCodeLink =
+    'https://github.com/zenorocha/react-email-apps/blob/main/campaigns/emails/2022-12-15-new-templates.tsx';
+
+  return (
+    <Html>
+      <Head />
+      <Preview>
+        The last few days have been crazy! We launched the React Email docs and
+        got a _lot_ of attention...
+      </Preview>
+      <Section style={main}>
+        <Text style={body}>Hey, it's Zeno Rocha.</Text>
+        <Text style={body}>The last few days have been crazy!</Text>
+        <Text style={body}>
+          We launched the React Email docs and got a _lot_ of attention. More
+          than 12k people visited the{' '}
+          <Link href={websiteLink}>new website</Link>, and we crossed 1.8k stars
+          on the <Link href={githubLink}>GitHub repo</Link>.
+        </Text>
+        <Link href="https://twitter.com/rauchg/status/1600151807707164672">
+          <Img
+            src="https://assets.react.email/campaigns/new-templates-tweet.png"
+            alt="react.email looks fire emoji - Guillermo Rauch"
+            width="400"
+            height="473"
+          />
+        </Link>
+        <Text style={body}>
+          Now we've been working hard to address all the issues and feature
+          requests that are coming in.
+        </Text>
+        <Text style={body}>
+          Thankfully, the community has been helping a lot, and they even
+          started to create a few new email templates.
+        </Text>
+        <Text style={body}>
+          Here's the <Link href={airbnbLink}>Airbnb template</Link> recreated by
+          @joaom00 and the <Link href={dropboxLink}>Dropbox template</Link>{' '}
+          recreated by @ribeiroevandro.
+        </Text>
+        <Link href={airbnbLink}>
+          <Img
+            src="https://assets.react.email/campaigns/new-templates-airbnb.png"
+            alt="Airbnb email template by @joaom00"
+            width="600"
+            height="386"
+          />
+        </Link>
+        <Text style={body}>
+          You can now find all the templates on{' '}
+          <Link href={examplesLink}>react.email/examples</Link>. And if you want
+          to contribute, here are{' '}
+          <Link href={contributeLink}>a couple of templates</Link> we still want
+          to implement using React Email.
+        </Text>
+        <Text style={body}>
+          Try it out and let me know if you have any questions :)
+        </Text>
+        <Text style={body}>Happy coding!</Text>
+        <Text style={body}>
+          P.S.: See the <Link href={sourceCodeLink}>source code</Link> of this
+          email written in React.
+        </Text>
+        <Hr />
+        <Text style={footer}>
+          You are receiving this email because you opted in at react.email
+        </Text>
+        <Text style={footer}>185 Royal Way - Upland, CA 91786</Text>
+      </Section>
+    </Html>
+  );
+};
+
+const fontFamily =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif";
+
+const main = {
+  backgroundColor: '#ffffff',
+};
+
+const body = {
+  fontFamily,
+};
+
+const footer = {
+  fontFamily,
+  color: '#898989',
+  fontSize: '12px',
+};
+
+export default Email;
